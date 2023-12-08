b8975fa9dfc00e907ffff77d11bfec674d2e3551
negisuraj080 <143009358+negisuraj080@users.noreply.github.com>
2023-12-08 13:18:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let i = shortHex.length - 3, isCapital = false;
+	let output = "#";
+	while (i < shortHex.length) {
+		let asciiCode = shortHex[i].charCodeAt(0);
+		if(asciiCode >= 65 && asciiCode <= 90){
+			isCapital = true;
+		}
+		output += shortHex[i] + shortHex[i];
+		i++
+	}
+	return output:
 };
 
-// Do not change the code below.
+Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
